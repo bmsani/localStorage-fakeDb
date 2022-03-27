943ae6ef362db549a7ba7c17c9591d311fd81781
bmsani <bmsani6@gmail.com>
2022-03-27 21:32:26 +0600

    localStorage setIem and getItem function Understood

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 let db = {};
 
 const addToDb = (item) => {
-    if(db[item]){
-        db[item] = db[item] + 1
+    const existData = localStorage.getItem(db)
+    if(existData){
+        db = JSON.parse(db)
     }
-    db[item] = 1;
+
+
+    if(item in db){
+        db[item] = db[item] + 1
+    }else{
+
+        db[item] = 1;
+    }
+    localStorage.setItem('setCart', JSON.stringify(db))
     console.log(db);
 }
 
 
-addToDb('linda')
-addToDb('pinda')
-addToDb('ginda')
-addToDb('linda')
+addToDb('kalu')
+addToDb('lalu')
+addToDb('kalu')
+addToDb('milu')
+addToDb('kalu')
